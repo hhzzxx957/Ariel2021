030f8ce615ee0eb7ddc0c167103177cd7c105753
Zhixing He <hezhx@deepblueai.com>
2021-07-12 16:29:59 +0800

    remove sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-NAME=resnet_featquan_preprocessed_adamw_cross
-mkdir -p outputs/$NAME
-nohup python -u train.py \
---save_name $NAME \
---log_dir $NAME \
---device_id 3 \
-> outputs/$NAME/output.log 2>&1 &
--- run_prediction.sh
+++
@@ -1,2 +0,0 @@
-Name=featquanphoton_proprocessed10_adamw_cross_seed1024
-nohup python -u prediction.py --save_name $Name --device_id 0 >> outputs/$Name/output.log 2>&1 &
